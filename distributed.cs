076ba365cc3d824aaaef9e2b04bd32a72a8c9c34
GabrielFim <gabifimtm@gmail.com>
2023-11-26 15:33:43 -0300

    TPP3

--- distributed.cs
+++ distributed.cs
@@ -300,94 +300,108 @@ namespace DotNetMPI
 
             MPI.Environment.Run(comm =>
             {
-                if (comm.Rank == 0)
-                {
-                    var inputFilePath = $"input_file_{size}.json";
-                    if (!File.Exists(inputFilePath))
-                        File.WriteAllText(inputFilePath, JsonSerializer.Serialize(Sequential.GenerateRandomIntArray(size).OrderByDescending(x => x)));
-                    var array = JsonSerializer.Deserialize<int[]>(File.ReadAllText(inputFilePath));
+                var sliceSize = Convert.ToInt32(Math.Floor((double)(size / comm.Size)));
 
-                    var sliceSize = Convert.ToInt32(Math.Floor((double)(array.Length / comm.Size)));
-
-                    var slice = array.Take(sliceSize).ToArray();
-                    comm.Send(slice, 0, 0);
-
-                    for (int i = 1; i <= comm.Size; i++)
-                    {
-                        slice = array.Skip(sliceSize * i).Take(sliceSize).ToArray();
-                        comm.Send(slice, i, 0);
-                    }
-
-                }
-                var received = comm.Receive<int[]>(0, 0);
+                var array = Sequential.GenerateRandomIntArray(sliceSize).ToArray();
 
                 Boolean finished = false;
+                int teste = 0;
 
                 while (!finished)
                 {
                     // ordeno vetor local
-                    var output = Sequential.BubbleSort(received);
+                    var output = Sequential.BubbleSort(array);
+                    Console.WriteLine($"Rank{comm.Rank}, array: {String.Join(", ", output)}");
 
                     // verifico condição de parada
+                    if (teste == 10)
+                    {
+                        finished = true;
+                    }
+                    // IDK
 
-
-                    // se não for np-1, mando o meu maior elemento para a direita
+                    //// se não for np-1, mando o meu maior elemento para a direita
                     if (comm.Rank != comm.Size - 1)
                     {
                         comm.Send(output.Last(), comm.Rank + 1, 0);
                     }
 
-                    // se não for 0, recebo o maior elemento da esquerda
+                    //// se não for 0, recebo o maior elemento da esquerda
                     var neighbour = 0;
                     if (comm.Rank != 0)
                     {
                         neighbour = comm.Receive<int>(comm.Rank - 1, 0);
                     }
 
-                    // comparo se o meu menor elemento é maior do que o maior elemento recebido (se sim, estou ordenado em relação ao meu vizinho)
-                    Boolean ordered = false;
-                    if (output.First() > neighbour)
+                    //// comparo se o meu menor elemento é maior do que o maior elemento recebido (se sim, estou ordenado em relação ao meu vizinho)
+                    var orderedToNeighbour = new bool[comm.Size];
+                    if (output.First() >= neighbour)
                     {
-                        ordered = true;
+                        orderedToNeighbour[comm.Rank] = true;
                     };
 
-                    // compartilho o meu estado com todos os processos
-                    /////// MPI_Bcast(ordered);
+                    //// compartilho o meu estado com todos os processos
+                    //MPI_Bcast(orderedToNeighbour);
 
-                    // se todos estiverem ordenados com seus vizinhos, a ordenação do vetor global está pronta ( pronto = TRUE, break)
+                    for (int i = 0; i < comm.Size; i++)
+                    {
+                        comm.Broadcast(ref orderedToNeighbour[i], i);
+                    }
 
-                    // senão continuo
+                    //// se todos estiverem ordenados com seus vizinhos, a ordenação do vetor global está pronta ( pronto = TRUE, break)
+                    if (orderedToNeighbour.All(x => x == true))
+                    {
+                        Console.WriteLine($"BREAK Rank{comm.Rank}");
+                        finished = true;
+                        break;
+                    }
+                    //// senão continuo
 
                     // troco valores para convergir
 
-                    // se não for o 0, mando os menores valores do meu vetor para a esquerda
-                    var slice = 5;
+                    //// se não for o 0, mando os menores valores do meu vetor para a esquerda
+                    var slice = 5; // Variar esse valor
                     if (comm.Rank != 0)
                     {
-                        comm.Send(output.Take(slice), comm.Rank - 1, 0);
+                        comm.Send(output.Take(slice).ToArray(), comm.Rank - 1, 0);
                     }
 
-                    // se não for np-1, recebo os menores valores da direita
+                    //// se não for np-1, recebo os menores valores da direita
                     var valuesNeighbour = new int[slice];
                     if (comm.Rank != comm.Size - 1)
                     {
-                        valuesNeighbour = comm.Receive<int[]>(comm.Rank - 1, 0);
+                        comm.Receive(comm.Rank + 1, 0, ref valuesNeighbour);
+
+                        ////// ordeno estes valores com a parte mais alta do meu vetor local
+                        var greaterSlice = output.Skip(output.Length - slice).Take(slice).ToArray();
+                        var combined = Interleaving(greaterSlice, valuesNeighbour);
+                        combined = Sequential.BubbleSort(combined);
+
+                        //Console.WriteLine($"Rank{comm.Rank}, combined: {String.Join(", ", combined)} \n");
+
+                        ////// Coloca os valores de volta no correto
+                        for (int i = 0; i < slice; i++)
+                        {
+                            array[array.Length - slice + i] = combined[i];
+                        }
+
+                        ////// devolvo os valores que recebi para a direita
+                        comm.Send(combined.Skip(slice).Take(slice).ToArray(), comm.Rank + 1, 0);
                     }
 
-                    // ordeno estes valores com a parte mais alta do meu vetor local
-
-
-                    // devolvo os valores que recebi para a direita
-                    if (comm.Rank != comm.Size - 1)
-                    {
-                        //comm.Send(IDK, comm.Rank + 1, 0);
-                    }
-
-                    // se não for o 0, recebo de volta os maiores valores da esquerda
+                    //// se não for o 0, recebo de volta os maiores valores da esquerda
+                    var valuesBack = new int[slice];
                     if (comm.Rank != 0)
                     {
-                        //comm.Receive<int[]>(comm.Rank + 1, 0);
+                        comm.Receive(comm.Rank - 1, 0, ref valuesBack);
+
+                        for (int i = 0; i < slice; i++)
+                        {
+                            array[i] = valuesBack[i];
+                        }
                     }
+
+                    teste++;
                 }
             });
         }
